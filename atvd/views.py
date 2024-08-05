0f5a642b942efa58d8fdedbf96ee532298805ee4
Diego <fdiegofreire@gmail.com>
2024-08-04 22:25:17 -0300

    ajuste de view e index

--- atvd/views.py
+++ atvd/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Home')
 
 def Diego(request):
     return render(request, 'Diego.html')
--- main/urls.py
+++ main/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from ..atvd.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/Diego', Diego),
-    path('/Heloisa', Heloisa)
+    path('', include('atvd.urls'))
 ]
